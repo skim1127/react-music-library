acd3fb7647cb9bcbb0fa7b24c33ab3fb3f10f72a
Jin mo Kim <skim112790@yahoo.com>
2022-03-22 21:15:52 -0400

    Added react router dom functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import { useState, useRef, Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Gallery from './components/Gallery.js';
+import SearchBar from './components/SearchBar.js';
 import './App.css';
+import { DataContext } from './context/DataContext.js';
+import { SearchContext } from './context/SearchContext.js';
+import AlbumView from './components/AlbumView.js'
+import ArtistView from './components/ArtistView.js';
 
 function App() {
+  // Define state variables
+  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState([])
+  let searchInput = useRef('')
+
+  const API_URL = 'https://itunes.apple.com/search?term='
+
+  // Defining function to pass down to the search bar
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    const fetchData = async () => {
+      document.title = `${ term } Music`
+      const response = await fetch(API_URL + term)
+      const resData = await response.json()
+      if (resData.results.length > 0) {
+        setData(resData.results)
+      } else {
+        setMessage('Not Found')
+      }
+    }
+    fetchData()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { message }
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <Fragment>
+
+              <SearchContext.Provider value={{
+                term: searchInput,
+                handleSearch: handleSearch
+                }}>
+                <SearchBar handleSearch={ handleSearch }/>
+              </SearchContext.Provider>
+
+              <DataContext.Provider value={ data }>
+                <Gallery data={ data }/>
+              </DataContext.Provider>
+              
+            </Fragment>
+          } />
+          <Route path="/album/:id" element={ <AlbumView/> } />
+          <Route path="artist/:id" element={ <ArtistView/> } />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Gallery.js
@@ -0,0 +1,21 @@
+import { useContext } from 'react'
+import { DataContext } from '../context/DataContext.js'
+import GalleryItem from './GalleryItem.js'
+
+function Gallery() {
+    const data = useContext(DataContext)
+
+    const display = data.map((item, index) => {
+        return(
+            <GalleryItem item={ item } key={ index }/>
+        )
+    })
+
+    return(
+        <div>
+            {display}
+        </div>
+    )
+}
+
+export default Gallery
